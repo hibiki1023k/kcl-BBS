41f0c097136961e6c3574476b7102dceeebd9e92
hibiki1023k <hibiki1023k@gmail.com>
2023-12-16 23:08:55 +0900

    data frontend setting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,52 @@
 // ~/src/app/page.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../components/header";
+import Modal from "../components/Modal";
 
 export default function Home() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("/api/posts")
+            .then((response) => {
+                if(!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setPosts(data);
+            })
+            .catch((error) => {
+                console.error("Error fetching posts:",error);
+            });
+    });
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleNewPost = (newPost) => {
+        setPosts([...posts, newPost]);
+    };
+
     return (
-        <div className="my-8">コンテンツ</div>
+        <>
+            <Header onOpenModal={handleOpenModal} />
+            {isModalOpen && <Modal closeModal={handleCloseModal} onNewPost={handleNewPost} />}
+            {/* 投稿を表示 */}
+            {posts.map((post, index) => (
+                <div key={index}>
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+                </div>
+            ))}
+    </>
     );
 }
