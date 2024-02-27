f32f0d7603e32d8be1d158a1aa2fe747589e3987
siloneco <siloneco4646@gmail.com>
2024-02-27 17:12:11 +0900

    もろもろ修正

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,132 +3,24 @@ import { ScrollArea } from '@/components/ui/scroll-area'
 
 export default function Page() {
   return (
-    <div className="grid grid-cols-12 gap-4">
-      <div className="bg-red-500 col-start-1 col-span-2 h-[80vh]"></div>
-      <div className="bg-blue-500 col-start-3 col-span-7 h-[80vh] ">
+    <div className="grid grid-cols-12 gap-2">
+      <div className="bg-red-300 col-start-1 col-span-2 h-[100vh]">1</div>
+      <div className="bg-blue-300 col-start-3 col-span-7 h-[100vh] ">
         <ScrollArea className="h-[100vh] p-2">
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="contentcontentcontent"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>{' '}
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
-          <div className="m-2">
-            <BBSCard
-              time={new Date()}
-              name="hoge"
-              content="content"
-              hashtag="学内のイベント"
-              avatarUrl="https://github.com/shadcn.png"
-            />
-          </div>
+          {[...Array(10)].map((_, i) => (
+            <div className="m-2" key={i}>
+              <BBSCard
+                time={new Date()}
+                name="hoge"
+                content="content "
+                hashtag="学内のイベント"
+                avatarUrl="https://github.com/shadcn.png"
+              />
+            </div>
+          ))}
         </ScrollArea>
       </div>
-      <div className="bg-purple-500 col-start-10 col-span-3 h-[80vh]">3</div>
+      <div className="bg-purple-300 col-start-10 col-span-3 h-[100vh]">3</div>
     </div>
-    // <div className="h-[100vh]">
-    //   <div className="w-[300px] mx-auto pt-10">
-    //     <BBSCard
-    //       time={new Date()}
-    //       name="hoge"
-    //       content="content"
-    //       hashtag="学内のイベント"
-    //       avatarUrl="https://github.com/shadcn.png"
-    //     />
-    //   </div>
-    // </div>
   )
 }
